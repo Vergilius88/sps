98fdb44240c2d606ba5736100e327725aa9944e4
Ihor_Nikolaienko <nikolasha8876@gmail.com>
2023-01-17 22:35:06 +0200

    strange commit 2

--- src/components/additionalCost/additionalCost.tsx
+++ src/components/additionalCost/additionalCost.tsx
@@ -14,8 +14,6 @@ export const AdditionalCost = () => {
                             <p></p>
                             <p>
                                 <span>
-
-                                    
                                 </span>
                             </p>
                         </div>
